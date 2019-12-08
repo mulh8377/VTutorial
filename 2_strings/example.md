e9e50827e116c6a259d6dd1b0dbd81cd36e7dacc
mulh8377 <vontossman@gmail.com>
2019-12-08 16:04:44 -0600

    more stuff

--- 2_strings/example.md
+++ 2_strings/example.md
@@ -1,10 +1,87 @@
-# vlib string.v
 ##  string_system_calls examples:
-	 - print: 	 	 	 	test4,test2,test3,test1
+print: 	 	 	 
+ 	test4,test2,test3,test1
+panic: 	 	 	 
+ 	ex: panic(ex)
+error: 	 	 	 
+ 	ex: error(ex)
+exit: 	 	 	 
+ 	ex: exit(ex.hash())
+
+##  string_delimiter_params ex
+split: 	 	 	 
+ 	["test4", "test2", "test3", "test1"]
+sort: 	 	 	 
+ 	["test1", "test2", "test3", "test4"]
+split_into_lines: 	 	 	 
+ 	["test4,test2,test3,test1"]
+join: 	 	 	 
+ 	test4,test2,test3,test1
+
+##  string_bool_return examples:
+contains: 	 	 	 
+ 	string:test4,test2,test3,test1
+ 	checking:test4
+ 	true
+starts_with: 	 	 	 
+ 	string:test4,test2,test3,test1
+ 	checking:test4
+ 	true
+ends_with: 	 	 	 
+ 	string:test4,test2,test3,test1
+ 	checking:test1
+ 	true
+isnil: 	 	 	 
+ 	string:test4,test2,test3,test1
+ 	checking:test4,test2,test3,test1
+ 	false
+
+##  string_heap_funcs examples:
+replace:test4,test2,test3,test1	 	 	 
+ 	with:stuff,to,split
+ 	stuff,to,split
+malloc: 	 	 	 
+ 	space:10	 	 	 
+ 	word:
+free: 	 	 	 
+ 	freed:size:10	 	 	 
  	
-	 - panic: 	 	 	 
- 		 ex: panic(ex)
-	 - error: 	 	 	 
- 		 ex: error(ex)
-	 - exit: 	 	 	 
- 		 ex: exit(ex.hash())
+##  string_void_params examples:
+to_upper: 	 	 	 
+ 	string:test4,test2,test3,test1
+ 	upper:TEST4,TEST2,TEST3,TEST1
+ 	
+to_lower: 	 	 	 
+ 	string:TEST4,TEST2,TEST3,TEST1
+ 	upper:test4,test2,test3,test1
+ 	
+trim: 	 	 	 
+ 	string:,,test2,test3,test1
+ 	after:test2,test3,test1
+ 	
+trim_space: 	 	 	 
+ 	string: test3,test1
+ 	after:test3,test1
+ 	
+
+## string_int_params examples:
+left: test4,test2,test3,test1	 	 	 
+ 	- not working
+right: test4,test2,test3,test1	 	 	 
+ 	- not working
+sub_str: test4,test2,test3,test1	 	 	 
+ 	 -not working
+
+ -##  string_int_returns examples:
+hash: test4,test2,test3,test1	 	 	 
+ 	
+ -value: 1771129662
+index: test4,test2,test3,test1	 	 	 
+ 	val: test4	 	 	 
+ 	 ---buggy
+last_index: test4,test2,test3,test1	 	 	 
+ 	val: test4	 	 	 
+ 	 ---buggy
+index_after: test4,test2,test3,test1	 	 	 
+ 	val: test4	 	 	 
+ 	 ---buggy
--- 2_strings/example.v
+++ 2_strings/example.v
@@ -7,38 +7,223 @@
 
 module main
 
-const (
-	HEADER = "# vlib string.v"
+import (
+	time
 )
 
+
 fn main() {
-	println(HEADER)
 	run()
 }
 
 fn run() {
-	ex := "test4,test2,test3,test1"
+	mut ex := "test4,test2,test3,test1"
 	string_system_calls(ex)
+	print('\n')
+
+	string_delimiter_params(ex)
+	print('\n')
+
+	string_bool_return(ex)
+	print('\n')
+
+
+	string_heap_funcs(ex)
+	print('\n')
+
+	string_void_params(ex)
+	print('\n')
+
+	string_int_params(ex)
+	print('\n')
+
+	string_int_returns(ex)
+
+
+	ex = "Finished"
+
+}
+
+pub fn string_int_params(ex string) {
+	println("## string_int_params examples:")
+	val := 4
+	print("left: " + ex + "\t \t \t \n \t")
+	//left_str := ex.left(4)
+	//print("after : + "left_str + "\t \t \t \n \t")
+	println("- not working")
+
+
+	print("right: " + ex + "\t \t \t \n \t")
+	//right_str := ex.right(4)
+	//print("after : + " ex.right(10) + "\t \t \t \n \t")
+	println("- not working")
+
+
+	print("sub_str: " + ex + "\t \t \t \n \t")
+	//sub_ex := ex.substr(4, 8)
+	//println(" -after :" + ex.substr(4, 8) + "\t \t \t \n \t")
+	println(" -not working")
+}
+
+fn string_int_returns(ex string) {
+
+	println(" -##  string_int_returns examples:")
+	p := "test4"
+	//hash function --> very nice !
+	print("hash: " + ex + "\t \t \t \n \t")
+	println("")
+	val := ex.hash()
+	println(" -value: " + val.str())
+	
+	print("index: " + ex + "\t \t \t \n \t")
+	print("val: " + p + "\t \t \t \n \t")
+	val2 := ex.index(p)
+	println(" ---buggy")
+
+
+	print("last_index: " + ex + "\t \t \t \n \t")
+	print("val: " + p + "\t \t \t \n \t")
+	val3 := ex.last_index(p)
+
+	//println(" -value: " + val.str())
+	println(" ---buggy")
+
+	print("index_after: " + ex + "\t \t \t \n \t")
+	print("val: " + p + "\t \t \t \n \t")
+	val4 := ex.index_after(p, 5)
+	println(" ---buggy")
+}
+
+fn string_delimiter_params(ex string) {
+
+	println("##  string_delimiter_params ex")
+
+	mut splt := ex.split(',')
+	print("split: \t \t \t \n \t")
+	println(splt)
+
+	splt.sort()
+	print("sort: \t \t \t \n \t")
+	println(splt)
+
+	lnes := ex.split_into_lines()
+	print("split_into_lines: \t \t \t \n \t")
+	println(lnes)
+
+	lnejoin := lnes.join(',')
+	print("join: \t \t \t \n \t")
+	println(lnejoin)
+	
+
+}
+
+fn string_bool_return(ex string) {
+	println("##  string_bool_return examples:")
+	strt := "test4"
+	endr := "test1"
+
+	print("contains: \t \t \t \n \t")
+	print("string:" + ex + "\n \t")
+	print("checking:" + strt + "\n \t")
+	println(ex.contains(strt))
+
+	print("starts_with: \t \t \t \n \t")
+	print("string:" + ex + "\n \t")
+	print("checking:" + strt + "\n \t")
+	println(ex.starts_with(strt))
+
+	print("ends_with: \t \t \t \n \t")
+	print("string:" + ex + "\n \t")
+	print("checking:" + endr + "\n \t")
+	println(ex.ends_with(endr))
+
+
+	print("isnil: \t \t \t \n \t")
+	print("string:" + ex + "\n \t")
+	print("checking:" + ex + "\n \t")
+	println(isnil(ex))
+
+}
+
+
+
+fn string_void_params(ex string) {
+	println("##  string_void_params examples:")
+
+	// to int function --> doesnt work?
+	//println(ex.to_())
+
+
+	print("to_upper: \t \t \t \n \t")
+	print("string:" + ex + "\n \t")
+	println('upper:' + ex.to_upper() + "\n \t")
+
+	ex_up := ex.to_upper()
+
+	print("to_lower: \t \t \t \n \t")
+	print("string:" + ex_up + "\n \t")
+	println('upper:' + ex_up.to_lower() + "\n \t")
+
+	
+	trimd := ex.replace('test4', ',')
+	print("trim: \t \t \t \n \t")
+	print('string:' + trimd + "\n \t")
+	println('after:' + trimd.trim(',') + '\n \t')
+
+	trimd_space := ex.replace('test4,test2,', ' ')
+
+	print("trim_space: \t \t \t \n \t")
+	print('string:' + trimd_space + "\n \t")
+	println('after:' + trimd_space.trim_space() + '\n \t')
+}
+
+fn string_heap_funcs(ex string) {
+	
+	println("##  string_heap_funcs examples:")
+
+	//replace a string
+	test := "stuff,to,split"
+	ex.replace(ex, test)
+	print("replace:" + ex + "\t \t \t \n \t")
+	print("with:" + test + "\n \t")
+	println(test)
+	
+	print("malloc: \t \t \t \n \t")
+	size := 10
+	word := malloc(size)
+	
+	print("space:" + size.str() + "\t \t \t \n \t")
+	print("word:" + word.str() + "\t \t \t \n \t")
+	println('')
+	time.sleep(1)
+
+	print("free: \t \t \t \n \t")
+	print("freed:" + word.str() + "\t \t \t \n \t")
+	print("size:" + size.str() + "\t \t \t \n \t")
+	//free(word)
+	
+	
 }
 
 fn string_system_calls(ex string) {
 	println("##  string_system_calls examples:")
-	print("\t - print: \t \t \t \t" + ex + '\n \t')
+	print("print: \t \t \t \n \t")
+	print(ex)
 	print('\n')
 
 
-	print("\t - panic: \t \t \t \n \t")
-	print("\t ex: panic(ex)")
+	print("panic: \t \t \t \n \t")
+	print("ex: panic(ex)")
 	print('\n')
 
-	print('\t - error: \t \t \t \n \t')
+	print('error: \t \t \t \n \t')
 	//op := error(ex)
-	print("\t ex: error(ex)")
+	print("ex: error(ex)")
 	print('\n')
 
-	print("\t - exit: \t \t \t \n \t")
+	print("exit: \t \t \t \n \t")
 	//exit(ex.hash())
-	print("\t ex: exit(ex.hash())")
+	print("ex: exit(ex.hash())")
 	//op = free(op)
 	print('\n')
 	
