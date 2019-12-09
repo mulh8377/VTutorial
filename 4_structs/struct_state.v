662fcf2a5a2d8d3ff8cdf339bc84bd5716330f88
mulh8377 <vontossman@gmail.com>
2019-12-08 18:14:23 -0600

    state machine kinda

--- 4_structs/struct_state.v
+++ 4_structs/struct_state.v
@@ -4,18 +4,30 @@ struct Edge {
 	dst string
 }
 
+struct State {
+	id string
+mut:
+	edges []Edge	
+}
+
+struct Machine {
+	id string
+mut:
+	states []State
+}
+
+struct MapMachine {
+mut:
+	keys []string
+	info map[string]Machine
+}
+
 fn initialize_edge(t string, s string, d string) Edge{
 	return Edge{token: t, src:s, dst: d}
 }
 
 fn print_edge(e Edge) string {
 	return '{token: $e.token, src: $e.src, dst: $e.dst}'
-}
-
-struct State {
-	id string
-mut:
-	edges []Edge	
 }
 
 fn initialize_state(i string) State {
@@ -52,19 +64,17 @@ fn print_state(s State) []string {
 	return res
 }
 
-struct Machine {
-	id int
-mut:
-	states []State
+fn initialize_machine(id string) Machine {
+	return Machine{id:id, states: []}
 }
 
-fn initialize_machine(id int) Machine {
-	return Machine{id:id, states: []}
+fn (m Machine) get_machine_id() string {
+	return m.id
 }
 
 fn (m Machine) print_machine() []string {
 	mut res := []string
-	res << m.id.str()
+	res << m.id
 	for s in m.states{
 		res << print_state(s)
 	}
@@ -75,6 +85,29 @@ fn (m Machine) print_machine() []string {
 
 fn (m mut Machine) add_state(s State) {
 	m.states << s
+}
+
+fn initialize_map_machine() MapMachine{
+	return MapMachine{}
+}
+
+fn (mapm mut MapMachine) add_to_map (m Machine) {
+	machine_hash := m.get_machine_id()
+	mapm.info[machine_hash] = m
+	mapm.keys << machine_hash
+	//println(mapm.info[machine_hash])
+}
+
+fn (mapm MapMachine) get_from_map (id string) Machine {
+	return mapm.info[id]
+}
+
+fn (mapm MapMachine) get_keys() [] string{
+	mut keys := []string
+	for k in mapm.keys {
+		keys << k
+	}
+	return keys
 }
 
 fn initialize_states() []State {
@@ -94,12 +127,22 @@ fn initialize_states() []State {
 }
 
 pub fn main() {
-	mut mach := initialize_machine(0)
+	mut mach := initialize_machine('0')
 	//mach.print_machine()
 	total_states := initialize_states()
 	for s in total_states {
 		mach.add_state(s)
 	}
 	info := mach.print_machine()
-	println(info)
+
+	println("creating a mapper for our machines! ")
+	mut mapper := initialize_map_machine()
+	mapper.add_to_map(mach)
+
+	test := mapper.get_from_map('0')
+	//getting the machine from the map!
+	println(test.print_machine())
+
+	// getting the keys from our maper
+	println(mapper.get_keys())
 }	
